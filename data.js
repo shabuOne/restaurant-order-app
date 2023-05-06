c34b5250aa6955f20e7553c709d8efcb30d12b86
Nils <nkrsneumann@outlook.de>
2023-05-06 13:54:17 +0200

    Ready Html Css Template

--- data.js
+++ data.js
@@ -2,22 +2,22 @@ const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
         price: 14,
-        emoji: "🍕"
+        image: "./images/pizza.png",
+        id: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
+        image: "./images/burger.png",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        emoji: "🍺",
+        image: "./image/beer.png",
         id: 2
     }
 ]
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import menuArray from "./data.js";
 
-document.getElementById('main-header').style.color = "red";
 console.log(menuArray)
